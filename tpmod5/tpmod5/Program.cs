2a1ddd07809b734ea1972b47ac91d3d07d92d869
reyhanhaidar <azanireyhan@student.telkomuniversity.ac.id>
2023-03-19 23:16:48 +0700

    menambahkan class generic

--- tpmod5/tpmod5/Program.cs
+++ tpmod5/tpmod5/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+namespace generic_method
+{
+    class HaloGeneric
+    {
+        public void SapaUSer()
+        {
+            String x = Console.ReadLine();
+            Console.WriteLine("Halo User " + x);
+        }
+    }
+
+   class main
+    {
+        static void Main(String[] args)
+        {
+            String x;
+            HaloGeneric halo = new HaloGeneric();
+            halo.SapaUSer();
+        }
+    }
+}
